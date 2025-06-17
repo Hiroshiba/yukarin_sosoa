fa06d3dd82016dfaaf07475e66bb22833542673e
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2025-06-17 12:57:12 +0900

    スペクトログラム生成にフィルタ機能追加

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,24 +1,30 @@
+"""
+データセットに対してモデルを使用してスペクトログラムを生成する。
+
+話者IDでフィルタリングすることも可能。
+"""
+
 import argparse
 import re
 from pathlib import Path
 from typing import Optional
 
 import numpy
+import torch
 import yaml
-from more_itertools import chunked
-from pytorch_trainer.dataset.convert import concat_examples
 from torch.utils.data.dataset import ConcatDataset
 from tqdm import tqdm
 
 from utility.save_arguments import save_arguments
 from yukarin_sosoa.config import Config
 from yukarin_sosoa.dataset import (
+    F0ProcessMode,
     FeatureTargetDataset,
     SpeakerFeatureDataset,
-    TensorWrapperDataset,
     create_dataset,
+    preprocess,
 )
-from yukarin_sosoa.generator import Generator
+from yukarin_sosoa.generator import Generator, GeneratorOutput
 
 
 def _extract_number(f):
@@ -28,7 +34,7 @@ def _extract_number(f):
 
 def _get_predictor_model_path(
     model_dir: Path,
-    iteration: int = None,
+    iteration: Optional[int] = None,
     prefix: str = "predictor_",
 ):
     if iteration is None:
@@ -44,15 +50,17 @@ def generate(
     model_dir: Path,
     model_iteration: Optional[int],
     model_config: Optional[Path],
-    time_second: float,
-    num_test: int,
+    dataset_name: str,
     output_dir: Path,
+    transpose: bool,
     use_gpu: bool,
+    target_speaker_ids: Optional[list[int]],
+    skip_existing: bool,
 ):
     if model_config is None:
         model_config = model_dir / "config.yaml"
 
-    output_dir.mkdir(exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
     save_arguments(output_dir / "arguments.yaml", generate, locals())
 
     config = Config.from_dict(yaml.safe_load(model_config.open()))
@@ -67,61 +75,92 @@ def generate(
         use_gpu=use_gpu,
     )
 
-    sampling_rate = 24000 / 512
+    config.dataset.test_num = 0
+    config.dataset.valid_num = 9999999
+    dataset = create_dataset(config.dataset)[dataset_name]
 
-    batch_size = config.train.batch_size
-
-    dataset = create_dataset(config.dataset)["test"]
     if isinstance(dataset, ConcatDataset):
         dataset = dataset.datasets[0]
-
     if isinstance(dataset.dataset, FeatureTargetDataset):
-        f0_paths = [inp.f0_path for inp in dataset.dataset.datas[:num_test]]
+        inputs = dataset.dataset.datas
+        speaker_ids = [None] * len(inputs)
     elif isinstance(dataset.dataset, SpeakerFeatureDataset):
-        f0_paths = [inp.f0_path for inp in dataset.dataset.dataset.datas[:num_test]]
+        inputs = dataset.dataset.dataset.datas
+        speaker_ids = dataset.dataset.speaker_ids
     else:
         raise ValueError(dataset)
 
-    for data, f0_path in zip(
-        chunked(tqdm(dataset, desc="generate"), batch_size),
-        chunked(f0_paths, batch_size),
-    ):
-        data = concat_examples(data)
-        specs = generator.forward(
-            f0=data["f0"],
-            phoneme=data["phoneme"],
-            speaker_id=data["speaker_id"] if "speaker_id" in data else None,
+    # 話者IDでフィルタリング
+    if target_speaker_ids is not None:
+        filtered_data = list(
+            filter(
+                lambda x: x[1] in target_speaker_ids,
+                zip(inputs, speaker_ids, strict=True),
+            )
         )
 
-        for spec, p in zip(specs, f0_path):
-            numpy.save(output_dir.joinpath(p.stem + ".npy"), spec)
+        # 対象データがない場合、trainならエラーを出し、それ以外ならワーニングを出す
+        if len(filtered_data) == 0:
+            message = f"データセット名 {dataset_name} にて、指定された話者ID {target_speaker_ids} に一致するデータが見つかりませんでした。"
+            if dataset_name == "train":
+                raise ValueError(message)
+            else:
+                print(message)
+                return
 
-    # validation
-    # for input, speaker_id in zip(
-    #     chunked(tqdm(inputs, desc="generate"), batch_size),
-    #     chunked(speaker_ids, batch_size),
-    # ):
-    #     # padding
-    #     f0_datas = [SamplingData.load(inp.f0_path) for inp in input]
-    #     f0 = numpy.stack(
-    #         [d.array[: int(time_length * f0_datas[0].rate)] for d in f0_datas]
-    #     )
+        inputs, speaker_ids = zip(*filtered_data)
 
-    #     phoneme_datas = [SamplingData.load(inp.phoneme_path) for inp in input]
-    #     phoneme = numpy.stack(
-    #         [d.array[: int(time_length * phoneme_datas[0].rate)] for d in phoneme_datas]
-    #     )
+    for input, speaker_id in tqdm(
+        zip(inputs, speaker_ids), total=len(inputs), desc="generate"
+    ):
+        name = Path(input.f0_path).stem
+        output_path = output_dir.joinpath(name + ".npy")
 
-    #     # generate
-    #     specs = generator.generate(
-    #         f0=f0,
-    #         phoneme=phoneme,
-    #         speaker_id=speaker_id if speaker_id[0] is not None else None,
-    #     )
+        if skip_existing and output_path.exists():
+            continue
 
-    #     # save
-    #     for spec, inp in zip(specs, input):
-    #         numpy.save(output_dir.joinpath(inp.f0_path.stem + ".npy"), spec)
+        input_data = input.generate()
+        data = preprocess(
+            d=input_data,
+            max_sampling_length=99999999,
+            prepost_silence_length=99999999,
+            f0_process_mode=F0ProcessMode(config.dataset.f0_process_mode),
+            time_mask_max_second=0,
+            time_mask_rate=0,
+        )
+
+        f0 = data["f0"]
+        phoneme = data["phoneme"]
+
+        # 長い場合は雑に区切る
+        if len(f0) > config.dataset.max_sampling_length:
+            num = len(f0) // config.dataset.max_sampling_length
+            f0_list = numpy.array_split(f0, num)
+            phoneme_list = numpy.array_split(phoneme, num)
+        else:
+            f0_list = [f0]
+            phoneme_list = [phoneme]
+
+        output_list: list[GeneratorOutput] = generator(
+            f0_list=f0_list,
+            phoneme_list=phoneme_list,
+            speaker_id=(
+                numpy.array([speaker_id] * len(f0_list))
+                if speaker_id is not None
+                else None
+            ),
+        )
+        spec = (
+            torch.cat([output["spec"] for output in output_list], dim=0)
+            .detach()
+            .cpu()
+            .numpy()
+        )
+
+        if transpose:
+            spec = spec.T
+
+        numpy.save(output_path, spec)
 
 
 if __name__ == "__main__":
@@ -129,8 +168,10 @@ if __name__ == "__main__":
     parser.add_argument("--model_dir", required=True, type=Path)
     parser.add_argument("--model_iteration", type=int)
     parser.add_argument("--model_config", type=Path)
-    parser.add_argument("--time_second", type=float, default=3)
-    parser.add_argument("--num_test", type=int, default=10)
+    parser.add_argument("--dataset_name", default="train")
     parser.add_argument("--output_dir", required=True, type=Path)
+    parser.add_argument("--transpose", action="store_true")
     parser.add_argument("--use_gpu", action="store_true")
+    parser.add_argument("--skip_existing", action="store_true")
+    parser.add_argument("--target_speaker_ids", type=int, nargs="+")
     generate(**vars(parser.parse_args()))
